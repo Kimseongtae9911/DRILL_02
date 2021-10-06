d194a6d81a8efc159cac753e592c4c80481067ee
KimSeongtae <seongtae07@gmail.com>
2021-10-06 17:22:45 +0900

    수업

--- Lecture06_HandlingInputs/move_character_with_mouse.py
+++ Lecture06_HandlingInputs/move_character_with_mouse.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
@@ -10,21 +11,36 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+        # elif event.type == SDL_MOUSEMOTION:
+        #     x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
     pass
 
+def update_character():
+    global x, y
+    global ax, ay
+    # x, y = ax, ay
+
+    t = 0.01
+    x = (1-t)*x + t*ax
+    y = (1-t)*y + t*ay
+
+    dist = (ax-x)**2 + (ay-y)**2
+    if dist < 10**2:
+        ax, ay = random.randint(0, KPU_WIDTH), random.randint(0, KPU_HEIGHT)
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+arrow = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+ax, ay = random.randint(0, KPU_WIDTH), random.randint(0, KPU_HEIGHT)
+
 frame = 0
 hide_cursor()
 
@@ -32,9 +48,12 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    arrow.draw(ax, ay)
     update_canvas()
     frame = (frame + 1) % 8
 
+    # 캐릭터 좌표 계산
+    update_character()
     handle_events()
 
 close_canvas()
